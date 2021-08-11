90f5f673acd88e014b48d2588284f25617343369
dmitriynovikov <dimusy@mail.ru>
2021-08-11 11:05:11 +0300

    home work 6

--- src/ru/gb/Main.java
+++ src/ru/gb/Main.java
@@ -3,5 +3,18 @@ package ru.gb;
 public class Main {
 
     public static void main(String[] args) {
+        int counter = 0;
+        boolean isBalansed;
+        for (int i = 0; i < 20; i++) {
+            TreeImpl<Integer>tree = new TreeImpl<>(6);
+            while (tree.add((int) ((Math.random()*200)-100))){}
+            tree.display();
+            isBalansed = tree.isBalanced(tree.getRoot());
+            System.out.println(isBalansed);
+            if (!isBalansed){
+                counter++;
+            };
+        }
+        System.out.println((float) counter/20*100 + "%");
     }
 }
---
+++ src/ru/gb/Node.java
@@ -0,0 +1,45 @@
+package ru.gb;
+
+public class Node<T extends Comparable<? super T>> {
+    private final T value;
+
+    private Node<T> leftChild;
+    private Node<T> rightChild;
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public Node<T> getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node<T> leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node<T> getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node<T> rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public boolean isLeftChild(T value) {
+        return value.compareTo(getValue()) < 0;
+    }
+
+    public boolean isLeaf() {
+        return leftChild == null && rightChild == null;
+    }
+
+    public boolean hasOnlyOneChild() {
+//        return (leftChild != null && rightChild == null) || (leftChild == null && rightChild != null);
+        return leftChild != null ^ rightChild != null;
+    }
+}
